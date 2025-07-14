f5093ffdc60df6a3a8ec6e519ba3e7dd8a674889
thulerjoao <thuler_lima@hotmail.com>
2025-07-14 15:53:19 -0300

    docs: initialized nest project with docker. It was also intaller swagger and zod libs

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('status')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get the status of the server' })
+  getAppStatus(): string {
+    return this.appService.getAppStatus();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { env } from './env';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, '..', 'client'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Recharge-Plataform_API')
+    .setDescription('API created by 4migaGames')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(env.PORT);
+}
+void bootstrap();
