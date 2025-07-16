a638bea34fd8fe2531b9a5fb0415add197926efd
thulerjoao <thuler_lima@hotmail.com>
2025-07-16 17:11:06 -0300

    code: created user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { StoreModule } from './store/store.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, StoreModule],
+  imports: [PrismaModule, StoreModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
